afd1836c31debd07f55d479be73269cf00f7932c
Oliver Benz <oliver.benz@outlook.com>
2019-08-27 09:28:04 +0200

    IMPLEMENTED: patch urls and dockerfile

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
+var cors = require('cors');
 
+var app = express();
+app.use(cors());
+app.use(bodyParser.json({ type: 'application/json' }));
+
+// Routers
+var dndRouter = express.Router();
+app.use('/dnd', dndRouter);
+
+// MySQL Setup
 var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -9,92 +19,119 @@ var connection = mysql.createConnection({
   database: 'dnd'
 });
 
-app.get('/getSpells', (req, res) => {
-  if(req.method == "GET"){
-    connection.query("SELECT * from spells", (err, result) => {
-      if(err) throw err;
 
-      res.set('Content-Type', 'application/json');
-      res.send(JSON.stringify(result));
-    });
-  }
-  else{
-    res.status(500);
-    res.send("Unexpected Method");
-  }
+
+// Get Spells
+dndRouter.get('/getSpells', (req, res) => {
+  connection.query("SELECT * from spells", (err, result) => {
+    if(err) throw err;
+
+    res.status(200);
+    res.set('Content-Type', 'application/json');
+    res.send(JSON.stringify(result));
+  });
 });
 
-app.get('/charGeneral/:sessionId/:charId', (req, res) => {
-  if(req.method == "GET"){
-    connection.query("SELECT c.firstname, c.lastname, c.level, c.xp, a.name, c.background, c.age, c.height, weight FROM characters c INNER JOIN alignments a ON a.id = c.alignment WHERE c.charString='" + req.params.charId + "' AND c.userId = (SELECT id from users WHERE sessionId = '" + req.params.sessionId + "')", (err, result) => {
-      if(err) throw err;  
+// -----------------------------------------
+//            Character General
+// -----------------------------------------
 
-      res.set('Content-Type', 'application/json');
-      res.send(JSON.stringify(result[0]));
-    });
-  }
-  else if(req.method == "POST"){
-    res.status(501);
-    res.send("Not implemented");
-  }
-  else{
-    res.status(500);
-    res.send("Unexpected case");
-  }
+dndRouter.get('/charGeneral/:sessionId/:charId', checkUserCharacter, (req, res) => {
+  connection.query("SELECT c.firstname, c.lastname, c.level, c.xp, a.name, c.background, c.age, c.height, weight FROM characters c INNER JOIN alignments a ON a.id = c.alignment WHERE c.charString='" + req.params.charId + "' AND c.userId = (SELECT id from users WHERE sessionId = '" + req.params.sessionId + "')", (err, result) => {
+    if(err) throw err;  
 
+    res.status(200);
+    res.set('Content-Type', 'application/json');
+    res.send(JSON.stringify(result[0]));
+  });
 });
 
-app.get('/charMoney/:sessionId/:charId', (req, res) => {
-  if(req.method == "GET"){
-    connection.query("SELECT copper, silver, electrum, gold, platinum from characters WHERE charString = '" + req.params.charId + "' AND userId = (SELECT id FROM users WHERE sessionId = '" + req.params.sessionId + "')", (err, result) => {
-      if(err) throw err;
-
-      res.set('Content-Type', 'application/json');
-      res.send(JSON.stringify(result[0]));
-    });
-  }
-  else if(req.method == "POST"){
-    res.status(501);
-    res.send("Not implemented");
-  }
-  else{
-    res.status(500);
-    res.send("Unexpected case");
-  }
+dndRouter.patch('/charGeneral/:sessionId/:charId', checkUserCharacter, (req, res) => {
+  connection.query("UPDATE characters SET xp", (err, result) => {
+    if(err) throw err;
   
+    // TODO: IMPLEMENT patch character XP and level(if XP at certain value)
+  });
 });
 
-app.get('/charHealth/:sessionId/:charId', (req, res) => {
-  if(req.method == "GET"){
-    connection.query("SELECT maxHealth, currentHealth, tempHealth from characters WHERE charString = '" + req.params.charId + "' AND userId = (SELECT id FROM users WHERE sessionId = '" + req.params.sessionId + "')", (err, result) => {
-      if(err) throw err;
 
-      res.set('Content-Type', 'application/json');
-      res.send(JSON.stringify(result[0]));
-    });
-  }
-  else if(req.method == "POST"){
-    res.status(501);
-    res.send("Not implemented");
-  }
-  else{
-    res.status(500);
-    res.send("Unexpected case");
-  }
-  
+// -----------------------------------------
+//            Character Money
+// -----------------------------------------
+
+dndRouter.get('/charMoney/:sessionId/:charId', checkUserCharacter, (req, res) => {
+  connection.query("SELECT copper, silver, electrum, gold, platinum from characters WHERE charString = '" + req.params.charId + "' AND userId = (SELECT id FROM users WHERE sessionId = '" + req.params.sessionId + "')", (err, result) => {
+    if(err) throw err;
+
+    res.status(200);
+    res.set('Content-Type', 'application/json');
+    res.send(JSON.stringify(result[0]));
+  });  
 });
 
-app.get('/charSpells/:sessionId/:charId', (req, res) => {
-  if(req.method == "GET"){
-    connection.query("SELECT s.* FROM charSpells c INNER JOIN spells s ON c.spellId = s.id WHERE c.characterId = (SELECT id from characters ch WHERE ch.charString = '" + req.params.charId + "' AND ch.userId = (SELECT id FROM users WHERE sessionId = '" + req.params.sessionId + "'))", (err, result) => {
-      if(err) throw error;
+dndRouter.patch('/charMoney/:sessionId/:charId', checkUserCharacter, (req, res) => {
+  connection.query("UPDATE characters SET copper = " + req.body.copper + ", silver = " + req.body.silver + ", electrum = " + req.body.electrum + ", gold = " + req.body.gold + ", platinum = " + req.body.platinum + " WHERE charString = '" + req.params.charId + "'", (err, result) => {
+    if(err) throw err;
 
-      res.set('Content-Type', 'application/json');
-      res.send(JSON.stringify(result));
-    });
-  }
+    res.status(200);
+    res.send("Update successful");
+  });  
 });
 
+
+// -----------------------------------------
+//            Character Health
+// -----------------------------------------
+
+dndRouter.get('/charHealth/:sessionId/:charId', checkUserCharacter, (req, res) => {
+  connection.query("SELECT maxHealth, currentHealth, tempHealth from characters WHERE charString = '" + req.params.charId + "' AND userId = (SELECT id FROM users WHERE sessionId = '" + req.params.sessionId + "')", (err, result) => {
+    if(err) throw err;
+
+    res.status(200);
+    res.set('Content-Type', 'application/json');
+    res.send(JSON.stringify(result[0]));
+  });
+});
+
+dndRouter.patch('/charHealth/:sessionId/:charId', checkUserCharacter, (req, res) => {
+  connection.query("UPDATE characters SET maxHealth = " + req.body.maxHealth + ", currentHealth = " + req.body.currentHealth + ", tempHealth = " + req.body.tempHealth + " WHERE charString = '" + req.params.charId + "'", (err, result) => {
+    if(err) throw err;
+
+    res.status(200);
+    res.send("Update successful");
+  });
+});
+
+
+// -----------------------------------------
+//            Character Spells
+// -----------------------------------------
+
+dndRouter.get('/charSpells/:sessionId/:charId', checkUserCharacter, (req, res) => {
+  connection.query("SELECT s.* FROM charSpells c INNER JOIN spells s ON c.spellId = s.id WHERE c.characterId = (SELECT id from characters ch WHERE ch.charString = '" + req.params.charId + "' AND ch.userId = (SELECT id FROM users WHERE sessionId = '" + req.params.sessionId + "'))", (err, result) => {
+    if(err) throw error;
+
+    res.status(200);
+    res.set('Content-Type', 'application/json');
+    res.send(JSON.stringify(result));
+  });
+});
+
+
+// Testing function
+function checkUserCharacter(req, res, next){
+  connection.query("SELECT id FROM characters WHERE charString='" + req.params.charId + "' AND userId = (SELECT id FROM users WHERE sessionId='" + req.params.sessionId + "')", (err, result) => {
+    if(err) throw err;
+
+    if(result.length === 0){
+      res.status(401);
+      res.send("Wrong CharacterId / SessionId");
+    }
+    else{
+      next();
+    }
+  });
+}
 
 
 app.listen(3004, () => {
