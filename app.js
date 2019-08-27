68317c7ad74fbfc9db1432f755fdfeb3ba30c919
Oliver Benz <oliver.benz@outlook.com>
2019-08-27 14:37:59 +0200

    fixed bcrypt error

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var bcrypt = require('bcrypt');
+var bcrypt = require('bcryptjs');
 
 var app = express();
 app.use(cors());
