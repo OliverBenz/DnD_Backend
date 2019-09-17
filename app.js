d4a922b8e11740bf96b5c211e7d7de7858d161f5
Oliver Benz <oliver.benz@outlook.com>
2019-09-17 09:10:43 +0200

    Fix db lost connection test

--- app.js
+++ app.js
@@ -13,19 +13,45 @@ app.use('/dnd', dndRouter);
 
 // MySQL Setup
 var mysql = require('mysql');
-// var connection = mysql.createConnection({
+// var dbconfig = {
 //   host: 'localhost',
 //   user: 'root',
 //   password: '',
 //   database: 'dnd'
-// });
-var connection = mysql.createConnection({
+// };
+
+var dbconfig = {
   host: '172.17.0.1',
   user: 'dnd',
   password: 'B71x!#rOWv$WH3&!2ltu43Y*nW3@7J',
   port: '3306',
   database: 'dnd'
-});
+};
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(dbconfig); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
 
 // -----------------------------------------
 //                 General
