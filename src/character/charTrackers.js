88677fdace15511e4a09a15a92be77646f006ed2
Oliver Benz <oliver.benz@outlook.com>
2019-09-23 08:12:27 +0200

    TEST: trackers

--- src/character/charTrackers.js
+++ src/character/charTrackers.js
@@ -6,13 +6,52 @@ exports.getCharTrackers = function(req, res){
     if(err){
       console.log(err);
 
-      res.status(500);
-      res.set('Content-Type', 'application/json');
-      res.send(JSON.stringify({ "success": false, "message": "Could not get Trackers" }));
+      send(res, 500, false, "Could not get Trackers", undefined);
     }
 
-    res.status(200);
-    res.set('Content-Type', 'application/json');
-    res.send(JSON.stringify({ "success": true, "data": result }));
+    send(res, 200, true, "", result);
   });
+}
+
+exports.addCharTracker = function(req, res){
+  connection.query("INSERT INTO charTrackers VALUES id, charId, title, value, maxValue, minValue", (err, result) => {
+    if(err){
+      console.log(err);
+
+      send(res, 500, false, "Could not add Tracker", undefined);
+    }
+
+    let data = {
+      "id": result.insertId,
+      "title": req.body.title,
+      "value": req.body.value,
+      "maxValue": req.body.maxValue,
+      "minValue": req.body.minValue
+    }
+    send(res, 200, true, "Successfully added Tracker", data)
+  });
+}
+
+exports.patchCharTracker = function(req, res){
+  connection.query("UPDATE charTracker SET value = value WHERE trackerId = trackerId AND charId = (SELECT id FROM characters WHERE charString = charString)", (err, result) => {
+    if(err){
+      console.log(err);
+
+      send(res, 500, false, "Could not update Tracker", undefined);
+    }
+
+    send(res, 200, true, "Successfully added Tracker", undefined);
+  });
+}
+
+function send(res, status, success, message, data){
+  if(data === undefined) data = [];
+  
+  res.status(status);
+  res.set('Content-Type', 'application/json');
+  res.send(JSON.stringify({
+    "success": success,
+    "message": message,
+    "data": data
+  }));
 }
