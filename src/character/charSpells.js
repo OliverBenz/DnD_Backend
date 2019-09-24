64983b648882a2ea31c49b86723a665431bb16d9
Oliver Benz <oliver.benz@outlook.com>
2019-09-24 09:35:45 +0200

    ADDED: get spells with limit for character

--- src/character/charSpells.js
+++ src/character/charSpells.js
@@ -5,6 +5,10 @@ exports.getCharSpells = function(req, res){
   connection.query("SELECT s.id, s.name, s.level, s.range FROM charSpells c INNER JOIN spells s ON c.spellId = s.id WHERE c.characterId = (SELECT id from characters ch WHERE ch.charString = '" + req.params.charString + "' AND ch.userId = (SELECT id FROM users WHERE sessionId = '" + req.params.sessionId + "'))", (err, result) => {
     if(err){
       console.log(err);
+
+      res.status(500);
+      res.set('Content-Type', 'application/json');
+      res.send(JSON.stringify({ "success": false, "message": "Could not get Character Spells" }));
     };
 
     res.status(200);
@@ -13,10 +17,33 @@ exports.getCharSpells = function(req, res){
   });
 }
 
+// Get Spells with Limit
+getCharSpellsLimit = function(req, res){
+  connection.query("SELECT s.id, s.name, s.level, s.range FROM charSpells c INNER JOIN spells s ON c.spellId = s.id WHERE c.characterId = (SELECT id from characters ch WHERE ch.charString = '" + req.params.charString + "' AND ch.userId = (SELECT id FROM users WHERE sessionId = '" + req.params.sessionId + "')) ORDER BY s.name ASC LIMIT " + req.params.limit + " OFFSET " + req.params.offset, (err, result) => {
+    if(err){
+      console.log(err);
+
+      res.status(500);
+      res.set('Content-Type', 'application/json');
+      res.send(JSON.stringify({ "success": false, "message": "Could not get Character Spells" }));
+    }
+
+    res.status(200);
+    res.set('Content-Type', 'application/json');
+    res.send(JSON.stringify({ "success": true, "data": result }));    
+  });
+}
+
 // Add new Spell to Char
 exports.postCharSpell = function(req, res){
   connection.query("INSERT INTO charSpells VALUES ((SELECT id FROM characters WHERE charString = '" + req.params.charString + "'), " + req.body.spellId + ")", (err, result) => {
-    if(err) console.log(err);
+    if(err) {
+      console.log(err)
+
+      res.status(500);
+      res.set('Content-Type', 'application/json');
+      res.send(JSON.stringify({ "success": false, "message": "Could not add spell to character" }));
+    };
 
     res.status(200);
     res.set('Content-Type', 'application/json');
@@ -28,7 +55,13 @@ exports.postCharSpell = function(req, res){
 // Delete Spell from Char
 exports.delCharSpell = function(req, res){
   connection.query("DELETE FROM charSpells WHERE characterId = (SELECT id FROM characters WHERE charString = '" + req.params.charString + "') AND spellId = " + req.body.spellId, (err, result) => {
-    if(err) console.log(err);
+    if(err) {
+      console.log(err)
+      
+      res.status(500);
+      res.set('Content-Type', 'application/json');
+      res.send(JSON.stringify({ "success": false, "message": "Could not remove spell from character" }));
+    };
 
     res.status(200);
     res.set('Content-Type', 'application/json');
@@ -42,6 +75,10 @@ exports.checkCharSpell = function(req, res){
   connection.query("SELECT * FROM charSpells WHERE characterId = (SELECT id FROM characters WHERE charString = '" + req.params.charString + "') AND spellId = " + req.params.spellId, (err, result) => {
     if(err){
       console.log(err); 
+
+      res.status(500);
+      res.set('Content-Type', 'application/json');
+      res.send(JSON.stringify({ "success": false, "message": "Could not check for spell" }));
     }
     
     if(result.length === 1){
