28c9e331ad509e68be6c98d3718d07990759fa22
OliverBenz <oliver.benz@outlook.com>
2020-07-02 17:38:32 +0200

    UPDATED: dbcon

--- src/dbcon.js
+++ src/dbcon.js
@@ -1,20 +1,18 @@
 // MySQL Setup
 var mysql = require('mysql');
-// var dbconfig = {
-//  host: 'localhost',
-//  user: 'dnd',
-//  password: 'B71x!#rOWv$WH3&!2ltu43Y*nW3@7J',
-//  port: '3306',
-//  database: 'dnd'
-// };
+var pool = require("./dbcon-pool.js").pool;
 
-var pool = mysql.createPool({
-  host: '172.17.0.1',
-  user: 'dnd',
-  password: 'B71x!#rOWv$WH3&!2ltu43Y*nW3@7J',
-  port: '3306',
-  database: 'dnd'
-});
+// Pool var was moved to dbcon-pool.js so no data is show publically
+// Pool var layout is as follows:
+//
+//var pool = mysql.createPool({
+//  host: '[db-ip (docker-standard 172.17.0.1)]',
+//  user: '[mysql-username]',
+//  password: '[mysql-password]',
+//  port: '[mysq-port (standard 3306]',
+//  database: '[databasename]'
+//});
+
 
 exports.query = function(sql, callback){
   pool.getConnection((err, connection) => {
